e2916712b4c5324465ceafe233a9ebd1d55ad0a2
monolok <antoinebertin@me.com>
2015-05-09 00:10:42 +0200

    pagination load more

---
+++ db/seeds.rb
@@ -0,0 +1,29 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+Post.create(title: "1")
+Post.create(title: "2")
+Post.create(title: "3")
+Post.create(title: "4")
+Post.create(title: "5")
+Post.create(title: "6")
+Post.create(title: "7")
+Post.create(title: "8")
+Post.create(title: "9")
+Post.create(title: "10")
+Post.create(title: "11")
+Post.create(title: "12")
+Post.create(title: "13")
+Post.create(title: "14")
+Post.create(title: "15")
+Post.create(title: "16")
+Post.create(title: "17")
+Post.create(title: "18")
+Post.create(title: "19")
+Post.create(title: "20")
+Post.create(title: "21")
+Post.create(title: "22")
+
